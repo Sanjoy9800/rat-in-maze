63bca9d7374b42b9b9a9bc46f2949371471541d2
Sanjoy9800 <sanjurudra4@gmail.com>
2022-12-08 19:41:17 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useContext } from "react";
+
+import "./components/Components.css";
+
+import { DataContext } from "./components/context/data-context";
+import Problem from "./components/Problem";
+import Solution from "./components/Solution";
 
 function App() {
+  const { arr, size } = useContext(DataContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="problem-container">
+        <h3>Initial Maze</h3>
+        <Problem arr={arr} size={size} />
+      </div>
+      <>
+        <Solution />
+      </>
     </div>
   );
 }
